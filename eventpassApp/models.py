c8cbb47b93ee2bc05113c81128fd2b3393c1d939
Arvind Suthar <arvindsuthar198@gmail.com>
2023-07-01 20:02:46 +0530

    Event model updated & migrations added

--- eventpassApp/models.py
+++ eventpassApp/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     title = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
-    organizer = models.CharField(max_length=50)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     starts_at = models.DateTimeField()
     ends_at = models.DateTimeField()
@@ -12,7 +11,8 @@ class Event(models.Model):
     pincode = models.IntegerField()
     category = models.CharField(max_length=50)
     description = models.TextField()
-    image_path = models.CharField(max_length=150)
+    image = models.FileField(upload_to="event-images")
+    ticket_price = models.IntegerField()
 
 class Booking(models.Model):
     event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
